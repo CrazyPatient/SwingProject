b800a48becbfec4033907f91d80eaf86fe1e465b
skkdkmw <skkdkmw@gmail.com>
2022-06-27 14:22:38 +0900

    테스트

--- SwingProjects/src/asdf.java
+++ SwingProjects/src/asdf.java
@@ -1,6 +1,7 @@
 
 public class asdf {
 	public static void main(String args[]) {
-		System.out.println("git kraken  asdf test");
+		
+		System.out.println("git kraken  asdf test");System.out.println("git kraken  asdf test");System.out.println("git kraken  asdf test");
 	}
 }
